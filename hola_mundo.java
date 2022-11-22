b446f7a795155bc2237a089ee92f5b0756ab6f56
CiscoSSJ <ciscocarande@gmail.com>
2022-11-22 21:22:13 +0100

    Fix conflict

--- hola_mundo.java
+++ hola_mundo.java
@@ -1,5 +1,16 @@
 public class hola_mundo {
 	public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello mundo");
 	}
 }
+=======
+        System.out.println("Hola mundo");
+  }
+  public static void imprimeSaludo() { 
+        System.out.println(“Hei”);
+	}
+}
+
+
+>>>>>>> 2bea031f33abc2ea872b85472edc6b01063ca61f
