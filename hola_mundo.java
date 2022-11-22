70877856fd291956a0fbaf92551e686f8cd7bf1e
Francisco Diaz Pozuelo <98550981+CiscoSSJ@users.noreply.github.com>
2022-11-22 21:25:24 +0100

    Update hola_mundo.java change "Hola" to "Wola"

--- hola_mundo.java
+++ hola_mundo.java
@@ -1,6 +1,6 @@
 public class hola_mundo {
 	public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Wola mundo");
     }
     public static void imprimeSaludo() { 
         System.out.println("Hei");
